98041620f412947bfab39d9a3c4d07cf7bb7262a
Ramses Alvarez <ramses.alvarez@tecsup.edu.pe>
2026-09-25 08:31:04 -0500

    -

--- Semana 05/OpcionA/app/src/main/java/com/alvarez/clinicasalud/model/ModelsAndRoutes.kt
+++ Semana 05/OpcionA/app/src/main/java/com/alvarez/clinicasalud/model/ModelsAndRoutes.kt
@@ -34,5 +34,5 @@ sealed class Screen(val route: String) {
     }
     object MyAppointments : Screen("my_appointments")
     object MedicalHistory : Screen("medical_history")
-    object UserProfile : Screen("user_profile") // <--- Nueva ruta añadida para el perfil del menú lateral
+    object UserProfile : Screen("user_profile")
 }
